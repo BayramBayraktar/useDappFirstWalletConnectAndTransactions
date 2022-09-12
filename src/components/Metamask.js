39ac2a23b541d8cadaf6048907c8456213852198
Bayram <57729264+bayram00@users.noreply.github.com>
2022-09-13 02:39:28 +0300

    first MetaMask project

---
+++ src/components/Metamask.js
@@ -0,0 +1,65 @@
+
+import { useEthers, useEtherBalance, useSendTransaction, ChainId, } from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
+
+const MetaMaskConnect = () => {
+    const { account, activateBrowserWallet, deactivate } = useEthers()
+
+    const EtherBalance = useEtherBalance(account)
+
+    const { sendTransaction, state } = useSendTransaction()
+
+    const disabled = ChainId === ChainId.Mainnet
+    const status = state.status
+    const address = '0xda4c3fc7C504A0E640820e45db047817cE659bCB'
+
+    const send = () => {
+        void sendTransaction({ to: address, value: 1 })
+    }
+
+
+
+    return (
+        <div >
+
+            <h1>WALLET</h1>
+            <div>
+                {
+                    account ?
+                        <button onClick={() => deactivate()}>Deacticate</button>
+                        :
+                        <button onClick={() => activateBrowserWallet()}>Connect</button>
+                }
+            </div>
+            <p>Acount = {account}</p>
+
+            {
+                EtherBalance &&
+                <p>{formatEther(EtherBalance)} Eth</p>
+            }
+
+            <hr />
+            <h1>Transctions</h1>
+            {
+                account &&
+                <div>
+                    {
+                        disabled ? (
+                            <p>Plase change the netwok from mainnet to proceed</p>
+                        )
+                            :
+                            (
+                                <>
+                                    <button onClick={() => send()}>Send ether</button>
+                                    <p>status :  {status} </p>
+                                </>
+                            )
+                    }
+
+                </div>
+            }
+        </div>
+    );
+}
+
+export default MetaMaskConnect;
